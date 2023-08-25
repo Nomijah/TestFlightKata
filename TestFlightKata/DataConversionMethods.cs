eb8134d9d6f99a5533c3280236aa7a0463c9779e
PetterBostrom <petter.bostrom@gmail.com>
2023-08-25 20:43:18 +0200

    Solved stage2b

--- TestFlightKata/DataConversionMethods.cs
+++ TestFlightKata/DataConversionMethods.cs
@@ -63,5 +63,71 @@ namespace TestFlightKata
             }
             return list;
         }
+
+        public static char ConvertConsonant(char c)
+        {
+            int charNumber = 0;
+            char newChar = 'a';
+            foreach(KeyValuePair<int, char> pair in charDict)
+            {
+                if (pair.Value == c)
+                {
+                    charNumber = pair.Key + 13;
+                    if (charNumber > 26)
+                    {
+                        charNumber -= 26;
+                    }
+                    foreach (KeyValuePair<int, char> pair2 in charDict)
+                    {
+                        if (pair2.Key == charNumber)
+                        {
+                            newChar = pair2.Value;
+                        }
+                    }
+                }
+            }
+            return newChar;
+        }
+
+        public static bool CheckIfConsonant(char a)
+        {
+            List<int> vowelKeyList = new List<int>
+            {
+                1,5,9,15,21,25
+            };
+
+            foreach (KeyValuePair<int, char> pair in charDict)
+            {
+                if (pair.Value == a)
+                {
+                    foreach (int i in vowelKeyList)
+                    {
+                        if (pair.Key == i)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static List<string> Stage2BConv(List<string> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                char[] temp = list[i].ToCharArray();
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (CheckIfConsonant(temp[j]))
+                    {
+                        temp[j] = ConvertConsonant(temp[j]);
+                    }
+                }
+                string convertedString = new string(temp);
+                list[i] = convertedString;
+            }
+            return list;
+        }
     }
 }
--- TestFlightKataTests/DataConversionMethodsTests.cs
+++ TestFlightKataTests/DataConversionMethodsTests.cs
@@ -41,6 +41,62 @@ namespace TestFlightKataTests
             Assert.Equal(actual, expected);
         }
 
+        [Theory]
+        [InlineData('P','C')]
+        [InlineData('L','Y')]
+        [InlineData('H','U')]
+        public static void ConvertConsonant_test(char a, char b)
+        {
+            // Given a character
+            char one = a;
+            char two = b;
+
+            // When converting to a character 13 steps after in the alphabet
+            char actual = ConvertConsonant(a);
+
+            // Expect correct conversion
+            char expected = two;
+            Assert.Equal(actual, expected);
+        }
+
+        [Theory]
+        [InlineData('A', false)]
+        [InlineData('K', true)]
+        [InlineData('T', true)]
+        public static void CheckIfConsonant_test(char a, bool b)
+        {
+            // Given a character
+            char test = a;
+
+            // When checking whether it is a consonant
+            bool actual = CheckIfConsonant(a);
+
+            // Expect result to be correct
+            bool expected = b;
+            Assert.Equal(actual, expected);
+
+        }
+
+        [Fact]
+        public void Stage2BConv_test()
+        {
+            // Given a list of strings
+            List<string> testList = new List<string>
+            {
+                "PONMLKJIHGFEDCBA", "FEDCBAZYXWVUTSRQ"
+            };
+
+            // When converting according to instructions
+            List<string> actual = Stage2BConv(testList);
+
+            // Expect the correct result
+            List<string> expected = new List<string>
+            {
+                "COAZYXWIUTSEQPOA", "SEQPOAMYKJIUGFED"
+            };
+            Assert.Equal(actual, expected);
+        }
+
         [Fact]
         public void CodeGenerator1_test()
         {
